2231b455ba3270c0647b2f66e5a8244ac74d0ef9
Loïc Boussiquet <loicboussiquet@MacBook-Pro-de-Loic.local>
2017-11-04 12:45:18 +0100

    Refactoring class

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+K
+src/main/java/Fizzbuzz.java,f/8/f89c9b19460d83114ef526d14ad44299c15fcbdd
+O
+src/test/java/FizzBuzzTest.java,c/4/c426cc44e2a557357656fa16d1411a13202f8905
--- src/main/java/Fizzbuzz.java
+++ src/main/java/Fizzbuzz.java
@@ -1,15 +1,15 @@
 public class Fizzbuzz {
 
-    public static String doFizzbuzz(int numbers) {
+    public String doFizzbuzz(int numbers) {
         String stringFizz = "";
         for (int i=1; i<=numbers; i++){
-            if((i%3)==0&&(i%5)==0){
+            if(divideBy3(i) && divideBy5(i)){
                 stringFizz += "FizzBuzz";
             }
-            else if((i%3)==0){
+            else if(divideBy3(i)){
                 stringFizz += "Fizz";
             }
-            else if((i%5)==0){
+            else if(divideBy5(i)){
                 stringFizz += "Buzz";
             }
             else {
@@ -20,5 +20,19 @@ public class Fizzbuzz {
             }
         }
         return stringFizz;
+    }
+
+    private boolean divideBy3 (int number){
+        if (number%3 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean divideBy5 (int number){
+        if (number%5 == 0){
+            return true;
+        }
+        return false;
     }
 }
