6300746b18b9057ac795032b8ae9780fec2d8d39
Loïc Boussiquet <loic333@hotmail.fr>
2017-11-04 13:04:06 +0100

    Refactoring class

--- src/main/java/Fizzbuzz.java
+++ src/main/java/Fizzbuzz.java
@@ -2,35 +2,39 @@ public class Fizzbuzz {
 
     public String doFizzbuzz(int numbers) {
         String stringFizz = "";
-        for (int i=1; i<=numbers; i++){
-            if(divideBy3(i) && divideBy5(i)){
-                stringFizz += "FizzBuzz";
-            }
-            else if(divideBy3(i)){
-                stringFizz += "Fizz";
-            }
-            else if(divideBy5(i)){
-                stringFizz += "Buzz";
-            }
-            else {
-                stringFizz += i;
-            }
-            if (i != numbers){
+        for (int i = 1; i <= numbers; i++) {
+            stringFizz += evaluateNumber(i);
+            if (i != numbers) {
                 stringFizz += " ";
             }
         }
         return stringFizz;
     }
 
-    private boolean divideBy3 (int number){
-        if (number%3 == 0){
+    private String evaluateNumber(int number) {
+        if (divideBy3(number) && divideBy5(number)) {
+            return "FizzBuzz";
+        }
+        if (divideBy3(number)) {
+            return "Fizz";
+        }
+        if (divideBy5(number)) {
+            return "Buzz";
+        }
+
+        return String.valueOf(number);
+
+    }
+
+    private boolean divideBy3(int number) {
+        if (number % 3 == 0) {
             return true;
         }
         return false;
     }
 
-    private boolean divideBy5 (int number){
-        if (number%5 == 0){
+    private boolean divideBy5(int number) {
+        if (number % 5 == 0) {
             return true;
         }
         return false;
